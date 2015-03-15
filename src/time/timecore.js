30d49f94e2978c068d158d7b0ef6dcce827c02fb
asissokho <abdoulaye.sissokho@hotmail.fr>
2015-03-16 00:07:01 +0100

    naming months correctly

--- src/time/timecore.js
+++ src/time/timecore.js
@@ -1,6 +1,16 @@
 (function (){
 
-    console.log('je suis dans le ficher src/time/timecore.js');
-    
+	var January = Jan =0,
+		February = Feb = 1,
+		March = Mar =  2,
+		April = Apr = 3,
+		May = 4,
+		June = Jun = 5,
+		July = Jul = 6,
+		August = Aug = 7,
+		September = Sep = 8,
+		October = Oct = 9,
+		November = Nov = 10,
+		December = Dec = 11;
 
 }());
