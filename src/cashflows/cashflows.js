fd46165a49a6e517402e607ae573ac23662d22b9
asissokho <abdoulaye.sissokho@hotmail.fr>
2015-03-17 22:42:59 +0100

    c

--- src/cashflows/cashflows.js
+++ src/cashflows/cashflows.js
@@ -1,8 +1,24 @@
 (function(){
 
-    function cashflows(){
+    function _hasOccured = function (settlementDate){
+        return function (cashflow){
+            if(!cashflow['hasOccured']){
+                var exCouponDays = cashflow.exCouponDays || 0,
+                    referenceDate = defaultCalendar.add( settlementDate, -1*exCouponDays);
+                return settlementDate < referenceDate;
+            }
+            return cashflows.hasOccured(settlementDate);
+        };
+    }
+
+    function _discount(yieldCurve, settlementDate){
+        return function (cashflow){
+            return yieldCurve.discountFactor(settlementDate, cashflow.paymentDate) * cashflow.amount;
+        }
+    }
+
+    var cashflows = function cashflows(){
         return new cashflow.fn.init();
-        
     };
 
     cashflows.fn = {
@@ -16,11 +32,10 @@
 
         accruedAmount : function(){
             
-        },
-
-        
+        }
     };
-    cashflows.prototype.accruedAmount= function (listOfCashflows, settlementDate){
+
+    cashflows.prototype.accruedAmount = function (listOfCashflows, settlementDate){
         // this is the type of code that we write a lot, I wanted to get it factorized 
         // to its more simplistic form
         var i=0,
@@ -32,27 +47,20 @@
         return accruedAmount;
     };
 
-    cashflows.prototype.npv = function (lisOfCashflows, yiedCurve, settlementDate){
-         var i=0,
-            n = listOfCashflows.length,
-
-            npv = 0;
-        for(;i<n;i++){
-           npv+=yieldCurve.discountFactor(listOfCashflows[i].date)*listOfCashflows[i].value(settlementDate);
-        }        
-        return npv;
-
+    cashflows.prototype.npv = function (lisOfCashflows, yiedCurve, settlementDate, evaluationDate){
+         return listOfCashflows.filter(_hasOccured(yieldCurve, settlementDate)
+                .map(_discount(settlementDate, evaluationDate)).sum();
     };
 
 
 
-    cashflows.prototype.modifiedDuration= function (lisOfCashflows, yieldCurve, settlementDate){
+    cashflows.prototype.modifiedDuration = function (lisOfCashflows, yieldCurve, settlementDate){
     };
 
     cashflows.prototype.maccaulayDuration = function (listOfCashflows, yieldCurve, settlementDate){
     };
 
-    cashflows.prototype.simpleDuration= function (listOfCashflows, yieldCurve, settlementDate){
+    cashflows.prototype.simpleDuration = function (listOfCashflows, yieldCurve, settlementDate){
        var i=0,
             n = listOfCashflows.length,
             npv = 0;
